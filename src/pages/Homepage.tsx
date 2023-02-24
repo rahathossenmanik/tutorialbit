b9151357c9f70d0f5d7255b0a4dca7a32d88d765
Manik Hosen <rahathossenmanik@gmail.com>
2023-02-24 22:05:21 +0600

    feat: home page post rendered successfully

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,16 +1,92 @@
+import { Col, List, Row } from 'antd';
 import React, { useEffect, useState } from 'react';
+import { TYPEWISE_ARCHIVE_ENDPOINTS } from '../constants/apis/typewiseArchiveEndpoints';
+import { IPost } from '../interfaces/IPost';
+import LoadingPage from './common/LoadingPage';
 
 const Homepage = () => {
-  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [posts, setPosts] = useState<IPost[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const [totalRows, setTotalRows] = useState<number>(0);
 
   useEffect(() => {
-    fetch(process.env.REACT_APP_BASE_URL + '/wp-json/wp/v2/posts')
-      .then((response) => response.json())
-      .then((json) => setPosts(json));
-  }, []);
-  console.log(posts);
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(
+          process.env.REACT_APP_BASE_URL +
+            TYPEWISE_ARCHIVE_ENDPOINTS.posts(currentPage)
+        );
+        const data = await res.json();
+        const totalPages = res.headers.get('x-wp-totalpages');
+        const totalRows = res.headers.get('x-wp-total');
+        setPosts(data);
+        setTotalPages(Number(totalPages));
+        setTotalRows(Number(totalRows));
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    };
+    fetchPosts();
+  }, [currentPage]);
+  console.log({ posts, totalPages, totalRows });
 
-  return <div>Homepage</div>;
+  return loading ? (
+    <LoadingPage />
+  ) : (
+    <>
+      <List
+        itemLayout='vertical'
+        size='large'
+        pagination={{
+          onChange: (page) => {
+            setCurrentPage(page);
+          },
+          total: totalRows,
+          current: currentPage,
+          pageSize: 5,
+          showSizeChanger: false,
+        }}
+        dataSource={posts}
+        renderItem={(post) => (
+          <List.Item key={post.link}>
+            <>
+              <List.Item.Meta
+                title={
+                  <a
+                    href={post.link}
+                    dangerouslySetInnerHTML={{
+                      __html: post?.title?.rendered,
+                    }}></a>
+                }
+              />
+              {
+                <Row gutter={[8, 8]}>
+                  <Col span={24} xl={8}>
+                    <img
+                      width={200}
+                      style={{ maxWidth: '100%' }}
+                      alt='logo'
+                      src='https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png'
+                    />
+                  </Col>
+                  <Col span={24} xl={16}>
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: post?.excerpt?.rendered,
+                      }}></div>
+                  </Col>
+                </Row>
+              }
+            </>
+          </List.Item>
+        )}
+      />
+    </>
+  );
 };
 
 export default Homepage;
