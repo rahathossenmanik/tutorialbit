c9b326bc7c5d9e05916363c3ff7b93520a987aa7
Manik Hosen <rahathossenmanik@gmail.com>
2023-02-25 13:46:46 +0600

    feat: menu dynamic

---
+++ src/constants/json/courses.ts
@@ -0,0 +1,70 @@
+export const courses = [
+  {
+    slug: '/courses/artificial-intelligence',
+    label: 'AI',
+  },
+  {
+    slug: '/courses/oop-c',
+    label: 'C++',
+  },
+  {
+    slug: '/courses/communication-engineering',
+    label: 'CE',
+  },
+  {
+    slug: '/courses/cisco-packet-tracer',
+    label: 'Cisco',
+  },
+  {
+    slug: '/courses/computer-simulation',
+    label: 'CSM',
+  },
+  {
+    slug: '/courses/database',
+    label: 'DBMS',
+  },
+  {
+    slug: '/courses/digital-image-processing',
+    label: 'DIP',
+  },
+  {
+    slug: '/courses/discrete-mathematics',
+    label: 'DM',
+  },
+  {
+    slug: '/courses/data-structure',
+    label: 'DS',
+  },
+  {
+    slug: '/courses/digital-signal-processing',
+    label: 'DSP',
+  },
+  {
+    slug: '/courses/java',
+    label: 'Java',
+  },
+  {
+    slug: '/courses/mathematics',
+    label: 'Math',
+  },
+  {
+    slug: '/courses/mpi-programming',
+    label: 'MPI',
+  },
+  {
+    slug: '/courses/networking',
+    label: 'Networking',
+  },
+  {
+    slug: '/courses/operating-system',
+    label: 'OS',
+  },
+  {
+    slug: '/about-us',
+    label: 'About',
+  },
+  {
+    slug: '/contact-us',
+    label: 'Contact',
+  },
+];
--- src/layout/Topbar.tsx
+++ src/layout/Topbar.tsx
@@ -3,6 +3,7 @@ import { Layout, Menu } from 'antd';
 import type { MenuProps } from 'antd';
 import { MenuOutlined } from '@ant-design/icons';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { courses } from '../constants/json/courses';
 const { Header } = Layout;
 
 const Topbar = () => {
@@ -17,91 +18,11 @@ const Topbar = () => {
         <img src={process.env.REACT_APP_LOGO_URL} alt='logo' height={20} />
       ),
     },
-    {
-      key: '/courses/artificial-intelligence',
-      label: 'AI',
-      onClick: () => navigate('/courses/artificial-intelligence'),
-    },
-    {
-      key: '/courses/oop-c',
-      label: 'C++',
-      onClick: () => navigate('/courses/oop-c'),
-    },
-    {
-      key: '/courses/communication-engineering',
-      label: 'CE',
-      onClick: () => navigate('/courses/communication-engineering'),
-    },
-    {
-      key: '/courses/cisco-packet-tracer',
-      label: 'Cisco',
-      onClick: () => navigate('/courses/cisco-packet-tracer'),
-    },
-    {
-      key: '/courses/computer-simulation',
-      label: 'CSM',
-      onClick: () => navigate('/courses/computer-simulation'),
-    },
-    {
-      key: '/courses/database',
-      label: 'DBMS',
-      onClick: () => navigate('/courses/database'),
-    },
-    {
-      key: '/courses/digital-image-processing',
-      label: 'DIP',
-      onClick: () => navigate('/courses/digital-image-processing'),
-    },
-    {
-      key: '/courses/discrete-mathematics',
-      label: 'DM',
-      onClick: () => navigate('/courses/discrete-mathematics'),
-    },
-    {
-      key: '/courses/data-structure',
-      label: 'DS',
-      onClick: () => navigate('/courses/data-structure'),
-    },
-    {
-      key: '/courses/digital-signal-processing',
-      label: 'DSP',
-      onClick: () => navigate('/courses/digital-signal-processing'),
-    },
-    {
-      key: '/courses/java',
-      label: 'Java',
-      onClick: () => navigate('/courses/java'),
-    },
-    {
-      key: '/courses/mathematics',
-      label: 'Math',
-      onClick: () => navigate('/courses/mathematics'),
-    },
-    {
-      key: '/courses/mpi-programming',
-      label: 'MPI',
-      onClick: () => navigate('/courses/mpi-programming'),
-    },
-    {
-      key: '/courses/networking',
-      label: 'Networking',
-      onClick: () => navigate('/courses/networking'),
-    },
-    {
-      key: '/courses/operating-system',
-      label: 'OS',
-      onClick: () => navigate('/courses/operating-system'),
-    },
-    {
-      key: '/about-us',
-      label: 'About',
-      onClick: () => navigate('/about-us'),
-    },
-    {
-      key: '/contact-us',
-      label: 'Contact',
-      onClick: () => navigate('/contact-us'),
-    },
+    ...courses.map((course) => ({
+      key: course.slug,
+      label: course.label,
+      onClick: () => navigate(course.slug),
+    })),
   ];
 
   useEffect(() => {
