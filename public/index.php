fd015f52e7e3b5e4052eeb72f5753da14aab4f76
Manik Hosen <rahathossenmanik@gmail.com>
2023-03-16 01:51:18 +0600

    ci: mdb import moved

--- public/index.php
+++ public/index.php
@@ -36,6 +36,21 @@
       async
       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6705737578975548"
       crossorigin="anonymous"></script>
+      <!-- Font Awesome -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+  rel="stylesheet"
+/>
+<!-- Google Fonts -->
+<link
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  rel="stylesheet"
+/>
+<!-- MDB -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.css"
+  rel="stylesheet"
+/>
 </head>
     <body>
     <noscript>
