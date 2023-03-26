d665befabe36d5e77c2a2bfa06b4c9769e15c4aa
Manik Hosen <rahathossenmanik@gmail.com>
2023-03-26 15:47:27 +0600

    feat: 404 to mdb

--- src/pages/common/Page404.tsx
+++ src/pages/common/Page404.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import { Result, Button } from 'antd';
 
 const Page404 = () => {
   document.title = '404 Not Found - Tutorial Bit';
   return (
     <>
-      <Result
-        status='404'
-        title='404'
-        subTitle='Page Not Found!'
-        extra={
-          <a href='/'>
-            <Button type='primary'>Home</Button>
-          </a>
-        }
-      />
+      <div className='page-404'>
+        <div className='text-center'>
+          <h1>
+            <span id='digit1'>4</span>
+            <span id='digit2'>0</span>
+            <span id='digit3'>4</span>
+          </h1>
+          <h3 className='mb-3'>PAGE NOT FOUND</h3>
+          <button type='button' className='btn btn-primary'>
+            Return To Home
+          </button>
+        </div>
+      </div>
     </>
   );
 };
